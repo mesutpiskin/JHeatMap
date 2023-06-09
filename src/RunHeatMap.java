0e5bb79644341a24809af780440a41748d9a8df3
Guillaume Roussin <guillaume.roussin44@gmail.com>
2023-06-09 17:56:09 +0200

    Removed unused functions and updated part of the code with newer features.

--- src/RunHeatMap.java
+++ src/RunHeatMap.java
@@ -8,8 +8,11 @@ import javax.imageio.ImageIO;
 
 public class RunHeatMap {
 
+    public static final String INPUT_PATH = "C:\\Users\\mesutpiskin\\Desktop\\background.png";
+    public static final String OUTPUT_PATH = "C:\\Users\\mesutpiskin\\Desktop\\output.png";
+
     public static void main(final String[] args) {
-        List<Point> points = new ArrayList<Point>();
+        List<Point> points = new ArrayList<>();
 
         for (int i = 0; i < 1000; i++) {
             int x = (int) (Math.random() * 200);
@@ -17,12 +20,11 @@ public class RunHeatMap {
             Point p = new Point(x,y);
             points.add(p);
         }
-        BufferedImage input = null;
-        final String originalImage = "C:\\Users\\mesutpiskin\\Desktop\\background.png";
-        input = loadImage(originalImage);
+
+        BufferedImage input = loadImage(INPUT_PATH);
         final JHeatMap myMap = new JHeatMap(points, input);
         
-        saveImage(myMap.createHeatMap(0.3f),"C:\\Users\\mesutpiskin\\Desktop\\output.png");
+        saveImage(myMap.createHeatMap(0.3f), OUTPUT_PATH);
     }
 
     public static BufferedImage loadImage(String ref) {
@@ -40,7 +42,7 @@ public class RunHeatMap {
             final File outputfile = new File(dest);
             ImageIO.write(buff, "png", outputfile);
         } catch (final IOException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
